5d7a00e84ee4826d86fe2a5f1fc2a1e0c5857342
WEN Yunze <y-wen@3sss.co.jp>
2026-06-25 18:01:50 +0900

    Add:[WEN] Add tests(Case09).

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f03_report/Case09.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f03_report/Case09.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f03_report;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,9 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+
+import jp.co.sss.lms.ct.util.WebDriverUtils;
 
 /**
  * 結合テスト レポート機能
@@ -35,14 +39,36 @@ public class Case09 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		//ウェブページのインスタンスを生成
+		String url = "http://localhost:8080/lms";//定数化
+		WebDriverUtils.goTo(url);
+		//画面内要素を確認
+		assertEquals("ログイン | LMS", WebDriverUtils.webDriver.getTitle(), "ログイン画面に遷移する");
+		assertNotNull(WebDriverUtils.webDriver.findElement(By.xpath("//input[@value='ログイン']")),
+				"ログイン画面にログインボタンが存在している");
+		//スクリーンショットを取得
+		WebDriverUtils.getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		// ログインしているアカウント情報を渡す
+		String ID = "StudentAA01";
+		String PW = "StudentAA1";
+		int waitTime = 120;
+		//ログインテスト正常系
+		WebDriverUtils.webDriver.findElement(By.id("loginId")).sendKeys(ID);
+		WebDriverUtils.webDriver.findElement(By.id("password")).sendKeys(PW);
+		WebDriverUtils.webDriver.findElement(By.xpath("//input[@value='ログイン']")).click();
+		WebDriverUtils.pageLoadTimeout(waitTime);//マジックナンバー防止
+		//画面内要素を確認
+		assertEquals("コース詳細 | LMS", WebDriverUtils.webDriver.getTitle(), "コース詳細画面に遷移する");
+		//スクリーンショットを取得
+		WebDriverUtils.getEvidence(new Object() {
+		});
 	}
 
 	@Test
