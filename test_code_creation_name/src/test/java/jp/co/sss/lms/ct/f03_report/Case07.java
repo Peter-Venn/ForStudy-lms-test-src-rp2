3359aff5c4bf34c9f8cbcdcd15d9008ef46dc0d2
WEN Yunze <y-wen@3sss.co.jp>
2026-06-25 17:53:50 +0900

    Delete:[WEN] delete RP2_10_結合テスト仕様書_1.1_文運澤.doc. Add:[WEN] Add RP2_10_結合テスト仕様書_1.2_文運澤.ods and tests(Case08). Fix:[WEN] Fix tests(Case07).

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f03_report/Case07.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f03_report/Case07.java
@@ -1,6 +1,9 @@
 package jp.co.sss.lms.ct.f03_report;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +12,10 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import jp.co.sss.lms.ct.util.WebDriverUtils;
 
 /**
  * 結合テスト レポート機能
@@ -35,35 +42,78 @@ public class Case07 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		//ウェブページのインスタンスを生成
+		String url = "http://localhost:8080/lms";//定数化
+		WebDriverUtils.goTo(url);
+		//画面内要素を確認
+		assertEquals("ログイン | LMS", WebDriverUtils.webDriver.getTitle(), "ログイン画面に遷移する");
+		assertNotNull(WebDriverUtils.webDriver.findElement(By.xpath("//input[@value='ログイン']")),
+				"ログイン画面にログインボタンが存在している");
+		//スクリーンショットを取得
+		WebDriverUtils.getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		// ログインしているアカウント情報を渡す
+		String ID = "StudentAA01";
+		String PW = "StudentAA1";
+		int waitTime = 120;
+		//ログインテスト正常系
+		WebDriverUtils.webDriver.findElement(By.id("loginId")).sendKeys(ID);
+		WebDriverUtils.webDriver.findElement(By.id("password")).sendKeys(PW);
+		WebDriverUtils.webDriver.findElement(By.xpath("//input[@value='ログイン']")).click();
+		WebDriverUtils.pageLoadTimeout(waitTime);//マジックナンバー防止
+		//画面内要素を確認
+		assertEquals("コース詳細 | LMS", WebDriverUtils.webDriver.getTitle(), "コース詳細画面に遷移する");
+		//スクリーンショットを取得
+		WebDriverUtils.getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 未提出の研修日の「詳細」ボタンを押下しセクション詳細画面に遷移")
 	void test03() {
-		// TODO ここに追加
+		//「未提出」研修にlocate
+		String pixel = "300";
+		WebDriverUtils.scrollBy(pixel);
+		List<WebElement> elements = WebDriverUtils.webDriver.findElements(By.xpath("//input[@value='詳細']"));
+		elements.get(2).click();
+		//画面内要素を確認
+		assertEquals("セクション詳細 | LMS", WebDriverUtils.webDriver.getTitle(), "セクション画面に遷移する");
+		//スクリーンショットを取得
+		WebDriverUtils.getEvidence(new Object() {
+		});
+
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「提出する」ボタンを押下しレポート登録画面に遷移")
 	void test04() {
-		// TODO ここに追加
+		//日報記入画面に遷移
+		WebDriverUtils.webDriver.findElement(By.xpath("//input[@value='日報【デモ】を提出する']")).click();
+		//スクリーンショットを取得
+		WebDriverUtils.getEvidence(new Object() {
+		});
+
 	}
 
 	@Test
 	@Order(5)
 	@DisplayName("テスト05 報告内容を入力して「提出する」ボタンを押下し確認ボタン名が更新される")
 	void test05() {
-		// TODO ここに追加
+		// 日報を記入して提出する
+		String text = "test";
+		WebDriverUtils.webDriver.findElement(By.xpath("//textarea[@class='form-control']")).sendKeys(text);
+		WebDriverUtils.webDriver.findElement(By.xpath("//button[@type='submit']")).click();
+		//スクリーンショットを取得
+		WebDriverUtils.getEvidence(new Object() {
+		});
 	}
 
 }
